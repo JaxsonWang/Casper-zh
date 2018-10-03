b6d713d647c6ea2c6f8c4055d68ff549efc600df
JaxsonWang <i@iiong.com>
2018-10-03 13:45:23 +0800

    添加新功能

--- assets/js/casper.js
+++ assets/js/casper.js
@@ -1,45 +1,3 @@
-var casper = {
-    // String 微博链接
-    weibo: "http://weibo.com/JaxsonWang",
-    // String QQ号码
-    qq: "1466469024",
-    // Boolean 百度推送
-    bdPush: true,
-    // String 百度统计ID
-    bdhmt: "37d3bf3116f041cb10bd1d890e65bcfc",
-    // String 页脚百度统计链接 例如：https://tongji.baidu.com/web/10921506/overview/index?siteId=12616599
-    footerBdhmt: "https://tongji.baidu.com/web/10921506/overview/index?siteId=11569818",
-    // String 备案号 例如：苏ICP备15050739号-4
-    footerBA1: "苏ICP备15050739号-4",
-    // String 备案号 例如：苏公网安备32010402000196号
-    footerBA2: "苏公网安备32010402000196号",
-    // String 备案号 网站描述
-    myDescription: "永远年轻，永远热泪盈眶",
-
-    // Boolean 是否开启valine评论支持
-    valine: false,
-    // String 参考：https://valine.js.org/quickstart.html#%E8%8E%B7%E5%8F%96APP-ID-%E5%92%8C-APP-Key
-    valineAppId: "rEDT0uBB2LEdndoJ4od2SlKf-gzGzoHsz",
-    // String 参考：https://valine.js.org/quickstart.html#%E8%8E%B7%E5%8F%96APP-ID-%E5%92%8C-APP-Key
-    valineAppKey: "lmX57j7hrYGCHROA72tBUIXq",
-    // Boolean 评论回复邮件提醒 参考：https://valine.js.org/notify.html
-    valineNotify: false,
-    // Boolean 验证码服务
-    valineVerify: false,
-    // String 评论框占位提示符
-    valinePlaceholder: "支持Markdown评论",
-
-    // Boolean Prism语法高亮
-    prism: true,
-    // Boolean Prism 展示行号
-    prismLineNumbers: true,
-    // Boolean Prism 展示语法类型
-    prismShowLanguage: true,
-    // Boolean Prism 粘贴剪切板
-    prismCopy: true,
-
-};
-
 var defaultCasper = {
     footerBA2URL: ""
 };
@@ -128,6 +86,12 @@ var vm = new Vue({
                 }
             }
         }
+        //图箱支持
+        if (this.mediumZoom) {
+            loadScript("//cdn.jsdelivr.net/npm/medium-zoom/dist/medium-zoom.min.js",function () {
+                mediumZoom(document.querySelectorAll(".post-full-content .post-content img"));
+            });
+        }
     }
 });
 
@@ -146,11 +110,6 @@ $("#return-to-top").click(function () {
         scrollTop: 0
     }, 500);
 });
-
-/**
- * 图箱配置
- */
-mediumZoom(document.querySelectorAll(".post-full-content .post-content img"));
 
 /**
  * 时间转换
