e31ac988db98e8e55d811298b8a19abca6119551
JaxsonWang <i@iiong.com>
2018-11-04 15:17:35 +0800

    添加新功能

--- assets/config.js
+++ assets/config.js
@@ -1,33 +1,33 @@
 var casper = {
     // String 微博链接
-    weibo: "http://weibo.com/JaxsonWang",
+    weibo: 'http://weibo.com/JaxsonWang',
     // String QQ号码
-    qq: "#",
+    qq: '',
     // Boolean 百度推送
     bdPush: true,
     // String 百度统计ID
-    bdhmt: "37d3bf3116f041cb10bd1d890e65bcfc",
+    bdhmt: '37d3bf3116f041cb10bd1d890e65bcfc',
     // String 页脚百度统计链接 例如：https://tongji.baidu.com/web/10921506/overview/index?siteId=12616599
-    footerBdhmt: "https://tongji.baidu.com/web/10921506/overview/index?siteId=11569818",
+    footerBdhmt: 'https://tongji.baidu.com/web/10921506/overview/index?siteId=11569818',
     // String 备案号 例如：苏ICP备15050739号-4
-    footerBA1: "苏ICP备15050739号-4",
+    footerBA1: '苏ICP备15050739号-4',
     // String 备案号 例如：苏公网安备32010402000196号
-    footerBA2: "苏公网安备32010402000196号",
+    footerBA2: '苏公网安备32010402000196号',
     // String 网站描述
-    myDescription: "永远年轻，永远热泪盈眶",
+    myDescription: '永远年轻，永远热泪盈眶',
     // String 支付宝二维码地址
-    aliPayDonate: "https://img.alicdn.com/imgextra/i4/2038135983/O1CN011u4G8M87EOv3N6Q_!!2038135983.jpg",
+    aliPayDonate: 'https://img.alicdn.com/imgextra/i4/2038135983/O1CN011u4G8M87EOv3N6Q_!!2038135983.jpg',
     // String 微信二维码地址
-    weChatDonate: "https://img.alicdn.com/imgextra/i1/2038135983/O1CN012t8d2E1u4G8KbRFYp_!!2038135983.png",
+    weChatDonate: 'https://img.alicdn.com/imgextra/i1/2038135983/O1CN012t8d2E1u4G8KbRFYp_!!2038135983.png',
 
     // Boolean 是否开启valine评论支持
     valine: true,
     // String 参考：https://valine.js.org/quickstart.html#%E8%8E%B7%E5%8F%96APP-ID-%E5%92%8C-APP-Key
-    valineAppId: "rEDT0uBB2LEdndoJ4od2SlKf-gzGzoHsz",
+    valineAppId: 'rEDT0uBB2LEdndoJ4od2SlKf-gzGzoHsz',
     // String 参考：https://valine.js.org/quickstart.html#%E8%8E%B7%E5%8F%96APP-ID-%E5%92%8C-APP-Key
-    valineAppKey: "lmX57j7hrYGCHROA72tBUIXq",
+    valineAppKey: 'lmX57j7hrYGCHROA72tBUIXq',
     // String 评论框占位提示符
-    valinePlaceholder: "支持Markdown评论",
+    valinePlaceholder: '支持Markdown评论',
 
     // Boolean Prism语法高亮
     prism: true,
